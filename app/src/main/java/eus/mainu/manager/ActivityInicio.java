da89fa78be29ef9a6f70fbaa97ba275a72c1d5ee
Gozaita <gozaita001@ikasle.ehu.eus>
2018-04-16 17:56:08 +0200

    Primeros pasos

--- app/src/main/java/eus/mainu/manager/ActivityInicio.java
+++ app/src/main/java/eus/mainu/manager/ActivityInicio.java
@@ -1,7 +1,10 @@
 package eus.mainu.manager;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class ActivityInicio extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class ActivityInicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        ProgressBar iniciando = findViewById(R.id.iniciando);
+        iniciando.getIndeterminateDrawable().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_IN);
     }
 }
